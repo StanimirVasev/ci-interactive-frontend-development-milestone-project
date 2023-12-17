44af0f377e700de37aeb9fed5be30537be1590ee
StanimirVasev <>
2023-12-17 20:51:41 +0000

    Added pokedex/instructions

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const volumeIcon = document.getElementById('volume');
     const volumeUpIcon = document.querySelector('.fa-volume-up');
     const volumeMuteIcon = document.querySelector('.fa-volume-mute');
+    const pokedexModal = document.getElementById('pokedexModal');
+    const pokedexIcon = document.querySelector('.pokedex');
+    const homeScreen = document.getElementById('home-screen');
+
+    // Hide the Pokedex modal initially
+    pokedexModal.style.display = 'none';
+    // Hide the volume mute icon initially
+    volumeMuteIcon.style.display = 'none';
 
     backgroundMusic.play();
 
@@ -15,6 +23,16 @@ document.addEventListener('DOMContentLoaded', function () {
             backgroundMusic.pause();
             volumeUpIcon.style.display = 'none';
             volumeMuteIcon.style.display = 'inline';
+        }
+    });
+
+    pokedexIcon.addEventListener('click', function () {
+        if (pokedexModal.style.display === 'none') {
+            pokedexModal.style.display = 'block';
+            homeScreen.style.display = 'none'; // Hide home-screen content
+        } else {
+            pokedexModal.style.display = 'none';
+            homeScreen.style.display = 'block'; // Show home-screen content
         }
     });
 });
